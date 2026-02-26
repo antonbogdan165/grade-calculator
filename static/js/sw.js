75d7a5151351161a0657b2bec7439f0f3fe197d9
Anton <antonbogdan165@gmail.com>
2026-02-26 16:19:45 +0500

    modified:   static/js/sw.js

--- static/js/sw.js
+++ static/js/sw.js
@@ -4,7 +4,7 @@
    Отдаётся Flask по маршруту /sw.js
    ============================================= */
 
-const CACHE_NAME = "bilimcalc-v1.2.1";
+const CACHE_NAME = "bilimcalc-v1.2.1b";
 
 const STATIC_ASSETS = [
     "/",
